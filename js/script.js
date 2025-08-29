3b4f5c3d9c0addea0d4a0ea9e8e52ce6059ccb8e
nargesnaghavi11@gmail.com <nargesnaghavi11@gmail.com>
2025-08-29 15:28:28 +0330

    first page (js)

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hamburg() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(0px)"; //نمایان میشه
+}
+
+function cancel() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(-500px)"; //نمایان میشه
+}
