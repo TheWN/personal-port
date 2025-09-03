7b5b65dc285e6fa4fb6cd8c9a6fc4e232a5bb3ee
nargesnaghavi11@gmail.com <nargesnaghavi11@gmail.com>
2025-09-03 22:54:07 +0330

    bugs
    image hasn't fixed yet
    the social links are vanishing

--- js/script.js
+++ js/script.js
@@ -5,5 +5,35 @@ function hamburg() {
 
 function cancel() {
   const navbar = document.querySelector(".dropdown");
-  navbar.style.transform = "translateY(-500px)"; //نمایان میشه
+  navbar.style.transform = "translateY(-500px)"; //قایم میشه
 }
+
+const text = ["DEVELOPER", "RESEARCHER", "DESIGNER", "BOXER"];
+
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textindex = 0;
+let charcterindex = 0;
+
+function typeWriter() {
+  if (charcterindex < text[textindex].length) {
+    textElements.innerHTML += text[textindex].charAt(charcterindex);
+    charcterindex++;
+    setTimeout(typeWriter, speed);
+  } else {
+    setTimeout(eraseText, 1000);
+  }
+}
+
+function eraseText() {
+  if (textElements.innerHTML.length > 0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+    setTimeout(eraseText, 50);
+  } else {
+    textindex = (textindex + 1) % text.length;
+    charcterindex = 0;
+    setTimeout(typeWriter, 500);
+  }
+}
+window.onload = typeWriter;
